c7afe89f87e9d84d1288e19b5160763bfb5c39fc
cristiane silva <criscage@hotmail.com>
2020-09-01 15:41:08 -0300

    criado, rota, controller e view de auction new

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -4,4 +4,30 @@ class AuctionsController < ApplicationController
     @auctions = Auction.all
   end
 
+  def new
+    @auction = Auction.new
+    @products = Product.all
+    @auction_product = AuctionProduct.new
+  end
+
+  def create
+    @auction = Auction.new(auction_params)
+    @auction.user = current_user
+    @auction.deadline = DateTime.now
+    @seleted_products = params[:products]
+  unless @selected_products.empty?
+    @auction.save
+    @selected_products.each do |product|
+    AuctionProduct.create(auction: @auction, product_id: product[:id], quantity: product[:quantity] )
+  end
+    redirect ??
+  else
+    render :new
+  end
+  end
 end
+  private
+
+  def auction_params
+    params.require(:auctions).permit(:status, :deadline, :product_id, :quantity)
+  end
