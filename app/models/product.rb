3863bfee3e308fae941aa2c5ec3de198eff60ef6
leandro barroso <lrbarroso1984@gmail.com>
2020-09-02 18:35:21 -0300

    Configs for photos and users params ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :auction_products
   has_many :bid_products
+  has_one_attached :photo
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,5 +2,6 @@ class Seller < ApplicationRecord
   belongs_to :user
   has_many :bids
   has_many :auctions, through: :bids
+  has_one_attached :photo
   validates :name, :address, presence: true
 end
