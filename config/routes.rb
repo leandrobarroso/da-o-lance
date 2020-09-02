a2b40efc17080b038a3e8eaab94f31eda20983c9
leandro barroso <lrbarroso1984@gmail.com>
2020-09-02 10:27:01 -0300

    coming soon page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-
+  root to: 'pages#home'
   devise_for :users
 
   resources :bids, only: [:index, :show]
 
   resources :auctions, only: [:index, :new, :create]
 
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
   resources :auctions do
     resources :sellers, except: :index do
       resources :bids
