492d378fd408208424d206dedc877aa7cc63e8b7
cristiane silva <criscage@hotmail.com>
2020-09-10 12:59:03 -0300

    ajustado loop home

--- app/javascript/plugins/init_typedjs.js
+++ app/javascript/plugins/init_typedjs.js
@@ -2,9 +2,9 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#typed', {
-    strings: ["Trazemos um novo jeito de fazer suas compras, crie suas listas de compras e receba ofertas em modelo leilão", "Trazemos um novo jeito de fazer suas compras, crie suas listas de compras e receba ofertas em modelo leilão"],
-    typeSpeed: 60,
-    loop: true
+    strings: ["Trazemos um novo jeito de fazer suas compras", "Trazemos um novo jeito de fazer suas compras"],
+    typeSpeed: 80,
+    loop: false
   });
 }
 
