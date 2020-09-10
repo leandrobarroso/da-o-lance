217d4120067bacd989ba379ea4566e8b98dbace3
cristiane silva <criscage@hotmail.com>
2020-09-10 12:03:51 -0300

    incluido borda avatar, ajustado altura botao iniciar, criado type.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,15 +25,17 @@ require("channels")
 import "bootstrap";
 import "cocoon";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initSelect2 } from '../plugins/init_select2';
 import { initFlatPickr } from "../plugins/init_flatpickr"
-
+import { loadDynamicBannerText } from '../plugins/init_typedjs';
 import { initComingSoon } from './init_coming_soon';
 
 import { initAuctionCable } from '../channels/auction_channel';
 import { initUserCable } from '../channels/user_channel';
+
 
 
 document.addEventListener('turbolinks:load', () => {
@@ -43,4 +46,5 @@ document.addEventListener('turbolinks:load', () => {
   initSelect2();
   initAuctionCable();
   initUserCable();
+  loadDynamicBannerText();
 });
