001319cabb658a1fd44811f9f18dd1d6fe82d386
cristiane silva <criscage@hotmail.com>
2020-09-10 12:15:58 -0300

    alteraçao frase home

--- app/javascript/plugins/init_typedjs.js
+++ app/javascript/plugins/init_typedjs.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#typed', {
-    strings: ["Trazemos um novo jeito de fazer suas compras", "Trazemos um novo jeito de fazer suas compras"],
+    strings: ["Trazemos um novo jeito de fazer suas compras, crie suas listas de compras e receba ofertas em modelo leilão", "Trazemos um novo jeito de fazer suas compras, crie suas listas de compras e receba ofertas em modelo leilão"],
     typeSpeed: 60,
     loop: true
   });
