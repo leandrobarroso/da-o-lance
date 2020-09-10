a3b19a7a390ef87dc021e83eb302294b48286dcf
ewertonteixeira10 <ewerton.steixeira@gmail.com>
2020-09-10 17:53:28 -0300

    Ajustes de CSS finais, JS da pagina inicial e correção de traduções

--- app/javascript/plugins/init_typedjs.js
+++ app/javascript/plugins/init_typedjs.js
@@ -2,9 +2,10 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   if (document.getElementById("typed")) {
+    const sentence = document.getElementById("sentence").innerText;
     new Typed('#typed', {
-    strings: ["Os melhores lances para suas listas de compras", "Os melhores lances para suas listas de compras"],
-    typeSpeed: 80,
+    strings: [sentence],
+    typeSpeed: 40,
     loop: false
     });
   }
